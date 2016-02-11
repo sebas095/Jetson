eb703394686531171f5816503eb5b40db404fce4
Sebastian Duque <sebas_tian_95@hotmail.com>
2016-02-10 22:35:49 -0500

    Sobel filter CPU/GPU

--- kernel.cu
+++ kernel.cu
@@ -15,9 +15,9 @@ void img2gray(unsigned char *imageInput, int width, int height, unsigned char *i
   int col = blockIdx.x*blockDim.x+threadIdx.x;
 
   if ((row < height) && (col < width)) {
-    imageOutput[row*width+col] = imageInput[(row * width + col) * 3 + RED] * 0.299 + \
-                                 imageInput[(row * width + col) * 3 + GREEN] * 0.587 + \
-                                 imageInput[(row * width + col) * 3 + BLUE] * 0.114;
+    imageOutput[row * width + col] = imageInput[(row * width + col) * 3 + RED] * 0.299 + \
+                                     imageInput[(row * width + col) * 3 + GREEN] * 0.587 + \
+                                     imageInput[(row * width + col) * 3 + BLUE] * 0.114;
   }
 }
 
