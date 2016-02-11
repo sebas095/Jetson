e2e1ad8148f6a62bc155f5e1e197918b136d45eb
Sebastian Duque <sebas_tian_95@hotmail.com>
2016-02-10 22:36:46 -0500

    Sobel filter CPU/GPU

--- kernel.cu
+++ kernel.cu
@@ -11,8 +11,8 @@ using namespace cv;
 
 __global__
 void img2gray(unsigned char *imageInput, int width, int height, unsigned char *imageOutput) {
-  int row = blockIdx.y*blockDim.y+threadIdx.y;
-  int col = blockIdx.x*blockDim.x+threadIdx.x;
+  int row = blockIdx.y * blockDim.y + threadIdx.y;
+  int col = blockIdx.x * blockDim.x + threadIdx.x;
 
   if ((row < height) && (col < width)) {
     imageOutput[row * width + col] = imageInput[(row * width + col) * 3 + RED] * 0.299 + \
